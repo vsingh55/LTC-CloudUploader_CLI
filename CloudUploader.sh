ebd00e4b2da2d7558c1eadd2af31bd9a2bebf660
krvsc <vskumar93@outlook.com>
2023-12-20 13:42:57 +0530

    updated cloud uploader script

--- CloudUploader.sh
+++ CloudUploader.sh
@@ -5,3 +5,11 @@ CLOUD_PROVIDER="azure"
 # Target container in the cloud (optional)
 TARGET_CONTAINER="blobcloudlocal"
 
+# Function to display usage information
+function display_usage {
+    echo "Usage: $0 <file_path> [options]"
+    echo "Options:"
+    echo "  -c, --container   Target container in the cloud"
+    echo "  -h, --help        Display this help message"
+}
+
