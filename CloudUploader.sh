6a820eb043c3f331700fa9dd5117e98a96f20dea
krvsc <vskumar93@outlook.com>
2023-12-21 09:24:39 +0530

    updated script

--- CloudUploader.sh
+++ CloudUploader.sh
@@ -3,7 +3,7 @@
 # Cloud provider
 CLOUD_PROVIDER="azure"
 # Target container in the cloud (optional)
-TARGET_CONTAINER="blobcloudlocal"
+TARGET_CONTAINER="" # Empty initially
 
 # Function to display usage information
 function display_usage {
@@ -13,3 +13,8 @@ function display_usage {
     echo "  -h, --help        Display this help message"
 }
 
+# Function to read user input
+function read_input {
+    read -p "$1: " INPUT
+    echo "$INPUT"
+}
